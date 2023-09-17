6ff3bfd966cbcd2d45b9d36a8b27b5b91c548928
ES-Yukun <yukun@doche.io>
2023-09-18 00:38:11 +0900

    update

--- example/test.c
+++ example/test.c
@@ -8,7 +8,8 @@ int	main(void)
 	char			c;
 
 	res = ctob('c');
+	printf("%d%d%d%d%d%d%d%d\n", res[0], res[1], res[2], res[3], res[4], res[5], res[6], res[7]);
 	c = btoc(res);
 	free(res);
-	printf("%c", c);
+	printf("%c\n", c);
 }
